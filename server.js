bf7dcdf5486929b694d1a97d07e9c26bc76c8147
arvindleadmaker022-wq <arvindleadmaker022@gmail.com>
2026-09-20 12:53:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import nodemailer from 'nodemailer';
 import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
-
+ 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
